ca19cece3a01a95081b4b8e664047edba644bcb5
alissonph <alissonphausmann@hotmail.com>
2021-08-03 15:35:26 -0300

    Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,9 @@
   <footer class="bg-light text-center">
     <div class="p-3">
       Desenvolvido em VueJS por
-      <a href="https://github.com/alissonph" target="_blank"
+      <a href="https://www.linkedin.com/in/alissonph/" target="_blank"
         >Alisson Hausmann</a
-      >
+      > | <a href="https://github.com/alissonph" target="_blank">GitHub</a>
     </div>
   </footer>
 </template>
